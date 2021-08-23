0790ca99bba5b11f927888c1fa3561bf5fbd94ff
Luana Gabriele de Sousa Costa <luanagabrielescosta@gmail.com>
2021-08-23 13:27:13 -0300

    Single folders deleted

--- the-fuse/server_mac/src/config/custom-express.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-const route = require('../app/controllers/main');
-route(app);
-
-// consign().include('src/app/controllers').into(app);
-
-module.exports = app;
