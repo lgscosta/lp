145cc17b832046d9e4a8c769a70dad7d585c0549
Luana Gabriele de Sousa Costa <luanagabrielescosta@gmail.com>
2021-08-24 16:58:52 -0300

    New deck code

--- the-fuse/trab/script.js
+++ the-fuse/trab/script.js
@@ -1,108 +1,66 @@
-import Deck from "./deck.js"
+// var cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+// var suits = ["diamonds", "hearts", "spades", "clubs"];
+var cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
+var deck = new Array();
 
-const CARD_VALUE_MAP = {
-  "2": 2,
-  "3": 3,
-  "4": 4,
-  "5": 5,
-  "6": 6,
-  "7": 7,
-  "8": 8,
-  "9": 9,
-  "10": 10,
-  J: 11,
-  Q: 12,
-  K: 13,
-  A: 14
+function getDeck()
+{
+	var deck = new Array();
+
+	// for(var i = 0; i < suits.length; i++)
+	// {
+		for(var x = 0; x < cards.length; x++)
+		{
+			var card = {Value: cards[x]};
+			deck.push(card);
+		}
+	// }
+
+	return deck;
 }
 
-const computerCardSlot = document.querySelector(".computer-card-slot")
-const playerCardSlot = document.querySelector(".player-card-slot")
-const computerDeckElement = document.querySelector(".computer-deck")
-const playerDeckElement = document.querySelector(".player-deck")
-const text = document.querySelector(".text")
+function shuffle()
+{
+	// for 1000 turns
+	// switch the values of two random cards
+	for (var i = 0; i < 1000; i++)
+	{
+		var location1 = Math.floor((Math.random() * deck.length));
+		var location2 = Math.floor((Math.random() * deck.length));
+		var tmp = deck[location1];
 
-let playerDeck, computerDeck, inRound, stop
+		deck[location1] = deck[location2];
+		deck[location2] = tmp;
+	}
 
-document.addEventListener("click", () => {
-  if (stop) {
-    startGame()
-    return
-  }
-
-  if (inRound) {
-    cleanBeforeRound()
-  } else {
-    flipCards()
-  }
-})
-
-startGame()
-function startGame() {
-  const deck = new Deck()
-  deck.shuffle()
-
-  const deckMidpoint = Math.ceil(deck.numberOfCards / 2)
-  playerDeck = new Deck(deck.cards.slice(0, deckMidpoint))
-  computerDeck = new Deck(deck.cards.slice(deckMidpoint, deck.numberOfCards))
-  inRound = false
-  stop = false
-
-  cleanBeforeRound()
+	renderDeck();
 }
 
-function cleanBeforeRound() {
-  inRound = false
-  computerCardSlot.innerHTML = ""
-  playerCardSlot.innerHTML = ""
-  text.innerText = ""
+function renderDeck()
+{
+	document.getElementById('deck').innerHTML = '';
+	for(var i = 0; i < 2; i++){
+	// for(var i = 0; i < deck.length; i++){
+		var card = document.createElement("div");
+		var value = document.createElement("div");
+		var suit = document.createElement("div");
+		card.className = "card";
+		value.className = "value";
+		suit.className = "suit " + deck[i].Suit;
 
-  updateDeckCount()
+		value.innerHTML = deck[i].Value;
+		card.appendChild(value);
+		card.appendChild(suit);
+
+		document.getElementById("deck").appendChild(card);
+	}
 }
 
-function flipCards() {
-  inRound = true
-
-  const playerCard = playerDeck.pop()
-  const computerCard = computerDeck.pop()
-
-  playerCardSlot.appendChild(playerCard.getHTML())
-  computerCardSlot.appendChild(computerCard.getHTML())
-
-  updateDeckCount()
-
-  if (isRoundWinner(playerCard, computerCard)) {
-    text.innerText = "Win"
-    playerDeck.push(playerCard)
-    playerDeck.push(computerCard)
-  } else if (isRoundWinner(computerCard, playerCard)) {
-    text.innerText = "Lose"
-    computerDeck.push(playerCard)
-    computerDeck.push(computerCard)
-  } else {
-    text.innerText = "Draw"
-    playerDeck.push(playerCard)
-    computerDeck.push(computerCard)
-  }
-
-  if (isGameOver(playerDeck)) {
-    text.innerText = "You Lose!!"
-    stop = true
-  } else if (isGameOver(computerDeck)) {
-    text.innerText = "You Win!!"
-    stop = true
-  }
+function load()
+{
+	deck = getDeck();
+	shuffle();
+	renderDeck();
 }
 
-function updateDeckCount() {
-  computerDeckElement.innerText = computerDeck.numberOfCards
-  playerDeckElement.innerText = playerDeck.numberOfCards
-}
-
-function isRoundWinner(cardOne, cardTwo) {
-  return CARD_VALUE_MAP[cardOne.value] > CARD_VALUE_MAP[cardTwo.value]
-}
-
-function isGameOver(deck) {
-  return deck.numberOfCards === 0
-}
+window.onload = load;
